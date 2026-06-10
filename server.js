3a0404851bb50772eef6d9b448d8612409f7c627
cityride558-pixel <cityride558@gmail.com>
2026-06-10 07:06:17 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -386,7 +386,7 @@ io.on('connection', (socket) => {
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
