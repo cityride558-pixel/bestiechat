e147d1761203fc47444a733c42a6046a926965a5
cityride558-pixel <cityride558@gmail.com>
2026-06-10 07:39:13 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 const server = http.createServer(app);
 const io = new Server(server, {
